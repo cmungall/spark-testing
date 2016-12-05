cd3f22d1337ade092f3bf0059c075d8bd4cfebd3
Chris Mungall <cjm@berkeleybop.org>
2016-12-05 09:30:35 -0800

    minor refactor

--- src/main/scala/Matchers.scala
+++ src/main/scala/Matchers.scala
@@ -4,7 +4,12 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import scala.math.sqrt
 
-case class Individual(id: Long, label: String, inferredTypes: Set[Long])
+case class Individual(
+    id: Long, 
+    label: String, 
+    directTypes : Set[Long],
+    inferredTypes: Set[Long]
+    )
 
 trait Matcher extends java.io.Serializable {
   def sim(i: Individual, j: Individual) : Double
@@ -27,6 +32,29 @@ class CosineMatcher extends Matcher {
   }
 }
 
+// TODO
+class BestMatchAverageMatcher extends Matcher {
+
+  def sim(i: Individual, j: Individual) : Double = {
+    val pairs = i.directTypes.map { 
+      c => (c, bestMatch(c))
+    }
+    
+    val totalScore = pairs.map( { x => x._2 } ).sum
+    
+    
+    
+    //i.directTypes
+    //val sumOfVectorProduct = i.inferredTypes.intersect(j.inferredTypes).size.toDouble
+    return totalScore / i.directTypes.size
+  }
+  
+  def bestMatch(classId: Long) : Double = {
+    return 0.0
+  }
+}
+
+
 object Util {
   def makeRandomIndividuals(numClasses: Integer, numIndividuals: Integer,
       numTypesPerIndividual: Integer) : Seq[Individual] = {
@@ -38,14 +66,16 @@ object Util {
   def makeRandomIndividual(id: Long, numClasses: Integer, numTypesPerIndividual: Integer) : Individual = {
     var r = new scala.util.Random
     val rtypes = 1 to numTypesPerIndividual map { _ => r.nextInt(100).toLong }
-    return Individual(id=id, label="", inferredTypes=rtypes.toSet)
+    return Individual(id=id, label="",
+        directTypes=Set(),
+        inferredTypes=rtypes.toSet)
   }
 }
 
 object JaccardTest {
   def main(args: Array[String]) {
-    val i = Individual(1L, "", Set(1L, 2L, 3L))
-    val j = Individual(2L, "", Set(2L, 3L, 4L))
+    val i = Individual(1L, "", Set(), Set(1L, 2L, 3L))
+    val j = Individual(2L, "", Set(), Set(2L, 3L, 4L))
     var m = new JaccardMatcher()
     
     println(s"FOO" + m.sim(i,j))
